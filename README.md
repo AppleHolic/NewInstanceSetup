16322b81f2970530e407ef2c278421b685a9fbf7
june-one <june.one@kakaobrain.com>
2017-12-28 13:21:05 +0900

    First Setup Script

---
+++ README.md
@@ -0,0 +1,7 @@
+#### Setup Scripts
+
+##### setup list
+1. bashrc for command line color
+2. vimrc
+3. screenrc
+4. /etc/apt/sources.list
---
+++ setup.sh
@@ -0,0 +1,10 @@
+# setup bashrc > color commandline
+cat bashrc_setting.txt >> ~/.bashrc
+# setup vimrc
+cp vimrc.txt ~/.vimrc
+# setup screenrc
+cp screen.txt ~/.screenrc
+# setup source repositories
+# backup 
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.backup
+sudo cp sources.list.txt > /etc/apt/sources.list
